09862c2c4d65971c07b65b55deaf848b42793212
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-08-04 13:40:18 +0530

    added optional catch-all route

--- app/api/users/endpoints.ts
+++ app/api/users/endpoints.ts
@@ -6,11 +6,12 @@ const User = Schema.Struct({
   name: Schema.String,
 });
 
-export const getUsers = HttpApiEndpoint.get("getUsers", "/").addSuccess(
-  Schema.Array(User)
-);
+export const getUsers = HttpApiEndpoint.get(
+  "getUsers",
+  "/api/users"
+).addSuccess(Schema.Array(User));
 
-export const getUser = HttpApiEndpoint.get("getUser", "/:id")
+export const getUser = HttpApiEndpoint.get("getUser", "/api/users/:id")
   .setPath(Schema.Struct({ id: Schema.NumberFromString }))
   .addSuccess(User, { status: 200 })
   .addError(HttpApiError.NotFound);
