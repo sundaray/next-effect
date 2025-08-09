33d64643252576bf44b0e5c0af96e6b68756b780
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-08-09 13:28:01 +0530

    migrate schema

--- lib/tools.ts
+++ lib/tools.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
 import { ToolSubmissionFormSchema } from "@/lib/schema";
 import { Effect, Schema, pipe, ParseResult } from "effect";
+import { DatabaseService } from "@/lib/services/database-service";
 
 const app = new Hono();
 
-app.post("/submit", async (ctx) => {
+app.post("/presigned-url", async (ctx) => {
   const body = await ctx.req.parseBody();
   const parsedBody = {
     ...body,
@@ -13,6 +14,8 @@ app.post("/submit", async (ctx) => {
 
   const program = Effect.gen(function* () {
     yield* Schema.decodeUnknown(ToolSubmissionFormSchema)(parsedBody);
+
+    // create a presigned URL and send back
 
     return ctx.json({
       message: "Tool submitted successfully!",
@@ -29,6 +32,18 @@ app.post("/submit", async (ctx) => {
   );
 
   return await Effect.runPromise(handledProgram);
+});
+
+app.post("/upload", async (ctx) => {
+
+
+  // Save the data in Supabase database
+  const program = Effect.gen(function* () {
+    const dbService = yield* DatabaseService;
+
+    const result = yield* dbService.use((db) => db.insert(tools). )
+
+  });
 });
 
 export default app;
